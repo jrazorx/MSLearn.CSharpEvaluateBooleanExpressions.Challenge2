fe5874f9e99d5c2ea9f5425af2c855e8a425ce85
jra <julien.gault.50@gmail.com>
2024-02-26 15:45:45 +0100

    compact solution

--- MSLearn.CSharpEvaluateBooleanExpressions.Challenge2/Program.cs
+++ MSLearn.CSharpEvaluateBooleanExpressions.Challenge2/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string permission = "Admin|Manager";
+int level = 55;
+bool isAdmin = permission.Contains("Admin");
+bool isManager = permission.Contains("Manager");
+
+/*
+ * This block evaluates the permission and level of the user
+ * It displays a message following these rules :
+ * Admin of level > 55      => Welcome, Super Admin user.
+ * Admin of level <=55      => Welcome, Admin user.
+ * Manager of level >= 20   => Contact an Admin for access.
+ * Any other case           => You do not have sufficient privileges.
+*/
+if (isAdmin)
+{
+    Console.WriteLine(level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.");
+}
+else if (isManager && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
